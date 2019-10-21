dc57fe10112525751f82208561f3d959836b65a5
roberGzt <rober.gerszen@gmail.com>
2019-10-20 21:56:19 -0300

    commite inicial

---
+++ src/main/java/com/somospnt/springreactordemo/SpringReactorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.somospnt.springreactordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorDemoApplication.class, args);
+	}
+
+}
